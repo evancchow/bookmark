50ce3d04af40373358f4f86318bb3ba3b97fc9d2
evancchow <echow@princeton.edu>
2014-11-15 14:25:55 -0500

    modulaizred the function in chrome.tab.query

--- bookmark/bookmark.js
+++ bookmark/bookmark.js
@@ -1,34 +1,29 @@
 
 
-// Run script. 
-var run_script = {
-    updateURL: function() {
-        chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, 
-            function (tabs) {
-                var siteURL = tabs[0].url;        
-                var url_txtbox = document.createElement('div');
-                var url_txt = "";
-                url_txt += encodeURI(siteURL);
-                url_txtbox.innerHTML = url_txt;
-                document.body.appendChild(url_txtbox);
-            });
-    }   
+updateURL = function() {
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, 
+        function(tabs) {
+            updateBox(tabs);
+        });
+}   
+
+updateBox = function(tabs) {
+        var siteURL = tabs[0].url;        
+        var url_txtbox = document.createElement('div');
+        var url_txt = "";
+        url_txt += encodeURI(siteURL);
+        url_txtbox.innerHTML = url_txt;
+        document.body.appendChild(url_txtbox);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    run_script.updateURL();
+    updateURL();
 
+    /* Here, feel free to load variables and things
+        from outside scripts (just put in HTML first). */
     var greeting = document.createElement('div');
     greeting.innerHTML = test_num; // load variable from outside script
     document.body.appendChild(greeting);
 
-
-
-    // Test pulling in a variable from another JS file
-    // $.getScript("test.js", function() {
-    //     alert("Loaded external script test.js!");
-    //     var mydiv = document.getElementbyID('test_field');
-    //     mydiv.innerText = "HI THERE";
-    // });
 });
 
