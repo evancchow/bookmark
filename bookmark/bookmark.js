e4b44991b655c88ed47416af153b0c566bbf22e5
evancchow <echow@princeton.edu>
2014-11-15 14:04:38 -0500

    Working on updating text outside of the textbox.

--- bookmark/bookmark.js
+++ bookmark/bookmark.js
@@ -1,24 +1,32 @@
-// chrome.tabs.query({'active' : true, 'lastFocusedWindow' : true}, function(tabs) {
-//     var url = tabs[0].url;
-// })
 
+
+// Run script. 
 var run_script = {
-
     updateURL: function() {
         chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, 
             function (tabs) {
                 var siteURL = tabs[0].url;        
-                var txt = document.createElement('div');
-                var final_txt = "";
-                final_txt += encodeURI(siteURL);
-                txt.innerHTML = final_txt;
-                document.body.appendChild(txt);
+                var url_txtbox = document.createElement('div');
+                var url_txt = "";
+                url_txt += encodeURI(siteURL);
+                url_txtbox.innerHTML = url_txt;
+                document.body.appendChild(url_txtbox);
             });
-
     }   
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     run_script.updateURL();
+
+    var greeting = document.createElement('div');
+    greeting.innerHTML = "hi durr";
+    document.body.appendChild(greeting);
+
+    // Test pulling in a variable from another JS file
+    // $.getScript("test.js", function() {
+    //     alert("Loaded external script test.js!");
+    //     var mydiv = document.getElementbyID('test_field');
+    //     mydiv.innerText = "HI THERE";
+    // });
 });
 
---
+++ bookmark/test.js
@@ -0,0 +1,1 @@
+var test_num = 1005;
